ccf8f51022faabb6a349ec7f2c6f8822d5dadf54
Sonia Klimas <sonia.klimas@gmail.com>
2020-07-22 12:52:11 +0200

    Updates

---
+++ precache-manifest.0dbcd37d25bfca00e7a5a8a414c75c18.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646cb62122f6667eb1dc880859c23dfc",
+    "url": "/Star_Wars_planet_encyclopedia/index.html"
+  },
+  {
+    "revision": "b56b0ee786250f2994db",
+    "url": "/Star_Wars_planet_encyclopedia/static/css/main.989af98f.chunk.css"
+  },
+  {
+    "revision": "2341b5c2b08a0723f303",
+    "url": "/Star_Wars_planet_encyclopedia/static/js/2.64f3d830.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Star_Wars_planet_encyclopedia/static/js/2.64f3d830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b56b0ee786250f2994db",
+    "url": "/Star_Wars_planet_encyclopedia/static/js/main.608312a3.chunk.js"
+  },
+  {
+    "revision": "3328c355cfbffeaf773e",
+    "url": "/Star_Wars_planet_encyclopedia/static/js/runtime-main.f97896f2.js"
+  },
+  {
+    "revision": "9f3a719add9d3b8ad4f8d2918acb5059",
+    "url": "/Star_Wars_planet_encyclopedia/static/media/ARROW CLOSE.9f3a719a.svg"
+  },
+  {
+    "revision": "4d66597d8badf6a02257a0704fa622fa",
+    "url": "/Star_Wars_planet_encyclopedia/static/media/ARROW OPEN.4d66597d.svg"
+  },
+  {
+    "revision": "8b6d494529d2a18c9a45450ed663dce9",
+    "url": "/Star_Wars_planet_encyclopedia/static/media/LOGO.8b6d4945.svg"
+  }
+]);
